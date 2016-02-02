85c99f77da643039deee008204762d1228525499
Vasil Vanchuk <v.vanchuk@tut.by>
2016-02-02 21:03:29 +0300

    Send followers/following with user data

--- app/controllers/user/list.js
+++ app/controllers/user/list.js
@@ -1,30 +1,77 @@
 module.exports = function(req, res, render) {
 
-  var findQuery;
+    var findQuery;
 
-  if (req.query.me && !req.user) {
-    return render("You're not logged in!");
-  }
-
-  if (req.query.username) {
-    findQuery = {
-      where: { username: req.query.username }
+    if (req.query.me && !req.user) {
+        return render("You're not logged in!");
     }
-  }
-  // Get the user from the token
-  if (req.query.me) {
-    findQuery = {
-      where: { id: req.user }
-    }
-  }
 
-  req.models.user
-  .findAll(findQuery)
-  .then(function(users) {
-    render(users);
-  })
-  .catch(function(err) {
-    render(err);
-  });
+    if (req.query.me) {
+        findQuery = {
+            where: {
+                id: req.user
+            },
+            include: [
+                req.models.chirp, {
+                    model: req.models.user,
+                    as: 'followees'
+                }, {
+                    model: req.models.user,
+                    as: 'followers'
+                }
+            ]
+        };
+    }
+
+    if (req.query.username) {
+        findQuery = {
+            where: {
+                username: req.query.username
+            },
+            include: [
+                req.models.chirp, {
+                    model: req.models.user,
+                    as: 'followees'
+                }, {
+                    model: req.models.user,
+                    as: 'followers'
+                }
+            ]
+        };
+    }
+
+    if (req.query.followee) {
+        findQuery = {
+            include: [{
+                model: req.models.user,
+                as: 'followees',
+                where: {
+                    username: req.query.followee
+                }
+            }]
+        };
+    }
+
+    if (req.query.follower) {
+        findQuery = {
+            include: [{
+                model: req.models.user,
+                as: 'followers',
+                where: {
+                    username: req.query.follower
+                }
+            }]
+        };
+    }
+
+    req.models.user
+        .findAll(findQuery)
+        .then(function(users) {
+            render(users);
+        })
+        .
+    catch (function(err) {
+        render(err);
+    });
 
 };
